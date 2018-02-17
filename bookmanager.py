8f1a04c6a8ee7a4f9ac97b553655ba8815d18c94
james947 <jmuriuki947@gmail.com>
2018-02-17 00:10:51 -0800

    Add postgres db URi

--- bookmanager.py
+++ bookmanager.py
@@ -1,7 +1,13 @@
 #import flask
 from flask import Flask, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 #initializing the app
 app = Flask(__name__)
+
+#connect to postgress db
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:8247@localhost/book_data'
+db = SQLAlchemy(app)
 
 #session seccret key
 app.config["SECRET_KEY"] ="b'e3ce76d4483bcce68ddf2067c443e43b87f02a304ab7a80e'"
