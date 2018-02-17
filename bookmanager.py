418d0c14bc5f8664342a72f2895cf80f5e76367d
james947 <jmuriuki947@gmail.com>
2018-02-16 21:14:50 -0800

    Add comments on the app

--- bookmanager.py
+++ bookmanager.py
@@ -1,11 +1,11 @@
+#import flask
 from flask import Flask, redirect, render_template
-
+#initializing the app
 app = Flask(__name__)
-
+#route to index page
 @app.route('/index/')
 def index():
-
     return render_template("index.html")
-
+#runs the app
 if __name__ == "__main__":
     app.run(debug=True)
