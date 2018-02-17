5b0f01c3044444de85c3c108b192ff4dd1f77e22
james947 <jmuriuki947@gmail.com>
2018-02-16 23:00:41 -0800

    Add secret key for session

--- bookmanager.py
+++ bookmanager.py
@@ -2,9 +2,14 @@
 from flask import Flask, redirect, render_template
 #initializing the app
 app = Flask(__name__)
+
+#session seccret key
+app.config["SECRET_KEY"] ="b'e3ce76d4483bcce68ddf2067c443e43b87f02a304ab7a80e'"
+
 #route to index page
-@app.route('/index/')
+@app.route('/index/', methods=['GET','POST'])
 def index():
+
     return render_template("index.html")
 #runs the app
 if __name__ == "__main__":
